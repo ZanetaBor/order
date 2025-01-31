8568a9587aa787a7d62fcd60ac1432c7b11bc208
Żaneta Bortnik <biurozanweb@gmail.com>
2025-01-31 14:49:10 +0100

    Update order.php

--- order.php
+++ order.php
@@ -31,11 +31,7 @@
 
 	
 	<meta http-equiv="Content-Security-Policy" default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self' data:;">
-	<link rel="icon" type="image/png" href="img/favicon-96x96.png" sizes="96x96" />
-	<link rel="icon" type="image/svg+xml" href="img/favicon.svg" />
-	<link rel="icon" type="image/x-icon" href="img/favicon.ico">
-	<link rel="manifest" href="img/site.webmanifest" /> 
-	    
+	
         <link rel="stylesheet" type="text/css" href="css/main.css">
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     </head>	
