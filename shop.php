9f9a4f4ba886d99fa5b0f43f8ba7706604fbfdd3
Żaneta Bortnik <biurozanweb@gmail.com>
2025-01-31 14:02:58 +0100

    Update shop.php

--- shop.php
+++ shop.php
@@ -7,7 +7,7 @@
         <meta name="keywords" content="order, shop">
         <title>Shop</title>
 
-	<meta http-equiv="Content-Security-Policy" content="default-src 'self'">
+	<meta http-equiv="Content-Security-Policy" default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'>
 	    
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <link rel="stylesheet" type="text/css" href="css/main.css">
