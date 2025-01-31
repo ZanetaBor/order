23dd7b1bcd884a8c78d4845c8035d2f0d3753f11
Żaneta Bortnik <biurozanweb@gmail.com>
2025-01-31 13:56:25 +0100

    Update order.php
    Content-Security-Policy

--- order.php
+++ order.php
@@ -28,6 +28,9 @@
         <meta name="keywords" content="order, shop">
 		<title>Podsumowanie zamowienia</title>
 		<meta name="viewport" content="width=device-width,initial-scale=1">
+
+	<meta http-equiv="Content-Security-Policy" content="default-src 'self'">
+	    
         <link rel="stylesheet" type="text/css" href="css/main.css">
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     </head>	
