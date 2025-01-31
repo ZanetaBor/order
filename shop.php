c7a45db8833230a07b11fb5320890604cefce647
Żaneta Bortnik <biurozanweb@gmail.com>
2025-01-31 14:32:58 +0100

    Update shop.php

--- shop.php
+++ shop.php
@@ -7,11 +7,10 @@
         <meta name="keywords" content="order, shop">
         <title>Shop</title>
 
-	<meta http-equiv="Content-Security-Policy" default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'>
+	<meta http-equiv="Content-Security-Policy" default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self' data:;">
 	<link rel="icon" type="image/png" href="img/favicon-96x96.png" sizes="96x96" />
 	<link rel="icon" type="image/svg+xml" href="img/favicon.svg" />
-	<link rel="shortcut icon" href="img/favicon.ico" />
-	<link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png" />
+	<link rel="icon" type="image/x-icon" href="img/favicon.ico">
 	<link rel="manifest" href="img/site.webmanifest" /> 
 	    
         <meta name="viewport" content="width=device-width,initial-scale=1">
